bbf898804926480c07849c9a7d579249ca807708
OneOh Cloud <github@oneoh.cloud>
2026-05-25 19:16:48 +0800

    updated

--- scripts/sync-version-ios.js
+++ scripts/sync-version-ios.js
@@ -19,24 +19,31 @@ const versionConfig = JSON.parse(fs.readFileSync('version.json', 'utf8'));
 const ver = versionConfig.version;
 const build = String(versionConfig.iosBuildNumber);
 
-const plistPath = path.join('ios', appName, 'Info.plist');
+const plistPaths = [
+  path.join('ios', appName, 'Info.plist'),
+  path.join('src', 'modules', 'expo-onebox', 'ios', 'OneBoxMTunnel', 'Info.plist'),
+];
 
-if (!fs.existsSync(plistPath)) {
-  console.error(`Error: ${plistPath} not found`);
-  process.exit(1);
+for (const plistPath of plistPaths) {
+  if (!fs.existsSync(plistPath)) {
+    console.error(`Error: ${plistPath} not found`);
+    process.exit(1);
+  }
 }
 
-let txt = fs.readFileSync(plistPath, 'utf8');
+for (const plistPath of plistPaths) {
+  let txt = fs.readFileSync(plistPath, 'utf8');
 
-txt = txt.replace(
-  /(<key>CFBundleShortVersionString<\/key>\s*<string>)[^<]*(<\/string>)/,
-  `$1${ver}$2`
-);
+  txt = txt.replace(
+    /(<key>CFBundleShortVersionString<\/key>\s*<string>)[^<]*(<\/string>)/,
+    `$1${ver}$2`
+  );
 
-txt = txt.replace(
-  /(<key>CFBundleVersion<\/key>\s*<string>)[^<]*(<\/string>)/,
-  `$1${build}$2`
-);
+  txt = txt.replace(
+    /(<key>CFBundleVersion<\/key>\s*<string>)[^<]*(<\/string>)/,
+    `$1${build}$2`
+  );
 
-fs.writeFileSync(plistPath, txt);
+  fs.writeFileSync(plistPath, txt);
+}
 console.log(`  CFBundleShortVersionString=${ver}, CFBundleVersion=${build}`);
